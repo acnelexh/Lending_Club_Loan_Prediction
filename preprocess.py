a324bb96a4a9a491464262945d845c5883866155
acnelexh <chenzyu@umich.edu>
2023-11-06 19:01:37 -0500

    finish clean up1st and encoding

--- preprocess.py
+++ preprocess.py
@@ -1,8 +1,9 @@
 import pandas as pd
 from pathlib import Path
+from sklearn import preprocessing
 
 # list of exportable functions
-__all__ = ['read_data', 'cleanup_df']
+__all__ = ['read_data', 'cleanup_df', 'encode_numerical', 'encode_categorical_cardinal']
 
 def read_data(path: Path) -> pd.DataFrame:
     '''
@@ -36,27 +37,79 @@ def cleanup_df(df: pd.DataFrame,
     df.dropna(axis=0, thresh=threshold_rows*len(df), inplace=True)
     return df
 
-def encode_categorical(df: pd.DataFrame, column_names: list) -> pd.DataFrame:
+def encode_categorical(df: pd.DataFrame, column_names: list, handle_nan: str = 'mode') -> pd.DataFrame:
     '''
     Encode categorical data
     args:
         df: pd.DataFrame
         column_names: list of column names
+        handle_nan: how to handle nan values
     return:
         pd.DataFrame
     '''
-    pass
+    onehot_encoder = preprocessing.OneHotEncoder()
+    categorical_df = df[column_names]
+    # replace nan with mode for each column
+    if handle_nan == 'mode':
+        categorical_df.fillna(categorical_df.mode(), inplace=True)
+    # encode categorical data
+    categorical_df = onehot_encoder.fit_transform(categorical_df)
+    return categorical_df, onehot_encoder.get_feature_names_out()
 
-def encode_numerical(df: pd.DataFrame, column_names: list) -> pd.DataFrame:
+def encode_categorical_ordinal(df: pd.DataFrame, column_names: list, order: list) -> pd.DataFrame:
+    '''
+    Encode categorical data
+    args:
+        df: pd.DataFrame
+        column_names: list of column names
+        handle_nan: how to handle nan values
+    return:
+        pd.DataFrame
+    '''
+    print(column_names)
+    ordinal_encoder = preprocessing.OrdinalEncoder(categories=order)
+    categorical_ordinal_df = df[column_names]
+    # replace nan with mode for each column
+    categorical_ordinal_df.fillna(categorical_ordinal_df.mode(), inplace=True)
+    # encode ordinal data
+    categorical_ordinal_df = ordinal_encoder.fit_transform(categorical_ordinal_df)
+    return categorical_ordinal_df, ordinal_encoder.get_feature_names_out()
+
+def encode_numerical(df: pd.DataFrame,
+                     column_names: list,
+                     scaling: str = 'standard',
+                     handle_nan: str = 'mean') -> pd.DataFrame:
     '''
     Encode numerical data
     args:
         df: pd.DataFrame
         column_names: list of column names
+        scaling: how to scale data (standard, minmax)
+        handle_nan: how to handle nan values (mean, median, mode)
     return:
         pd.Dataframe
     '''
-    pass
+    numerical_df = df[column_names]
+    # replace nan with mean for each column
+    if handle_nan == 'mean':
+        numerical_df.fillna(numerical_df.mean(), inplace=True)
+    elif handle_nan == 'median':
+        numerical_df.fillna(numerical_df.median(), inplace=True)
+    elif handle_nan == 'mode':
+        numerical_df.fillna(numerical_df.mode(), inplace=True)
+    # scale data
+    scaler = None
+    if scaling == 'standard':
+        scaler = preprocessing.StandardScaler()
+    elif scaling == 'minmax':
+        scaler = preprocessing.MinMaxScaler()
+    try:
+        numerical_df = scaler.fit_transform(numerical_df)
+    except ValueError:
+        print('Error while scaling numerical data')
+    except AttributeError:
+        print('Scaler not found')
+    return numerical_df
 
 def encode_target(df: pd.DataFrame, column_name: str) -> pd.DataFrame:
     '''
