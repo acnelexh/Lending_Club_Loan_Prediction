6ccd69ac3d970ad164d0f406f83decae0cc94407
acnelexh <chenzyu@umich.edu>
2023-12-08 15:12:01 -0500

    added model generalization through pipelining

--- model/deep_learning.py
+++ model/deep_learning.py
@@ -68,6 +68,68 @@ class LinearModel(L.LightningModule):
         optimizer = torch.optim.Adam(self.parameters(), lr=self.lr, weight_decay=self.weight_decay)
         return optimizer
 
+class LinearModelBatchNorm(LinearModel):
+    # same as LinearModel but with batch normalization
+    def __init__(self,
+                 input_size: int,
+                 n_classes: int,
+                 hidden_size: list,
+                 lr: float,
+                 weight_decay: float, **kwargs):
+        super().__init__(input_size, n_classes, hidden_size, lr, weight_decay)
+        # add batch normalization and replace linear layers
+        layers = []
+        for i in range(len(hidden_size)):
+            if i == 0:
+                layers.append(torch.nn.Sequential(Linear(input_size, hidden_size[i]), torch.nn.BatchNorm1d(hidden_size[i])))
+            else:
+                layers.append(torch.nn.Sequential(Linear(hidden_size[i-1], hidden_size[i]), torch.nn.BatchNorm1d(hidden_size[i])))
+        layers.append(Linear(hidden_size[-1], n_classes))
+        self.model = torch.nn.Sequential(*layers)
+
+class LinearModelDropout(LinearModel):
+    # same as LinearModel but with dropout
+    def __init__(self,
+                 input_size: int,
+                 n_classes: int,
+                 hidden_size: list,
+                 lr: float,
+                 weight_decay: float, **kwargs):
+        super().__init__(input_size, n_classes, hidden_size, lr, weight_decay)
+        # add batch normalization and replace linear layers
+        layers = []
+        for i in range(len(hidden_size)):
+            if i == 0:
+                layers.append(torch.nn.Sequential(Linear(input_size, hidden_size[i]), torch.nn.Dropout(0.2)))
+            else:
+                layers.append(torch.nn.Sequential(Linear(hidden_size[i-1], hidden_size[i]), torch.nn.Dropout(0.2)))
+        layers.append(Linear(hidden_size[-1], n_classes))
+        self.model = torch.nn.Sequential(*layers)
+
+class LinearModelBatchNormDropout(LinearModel):
+    # same as LinearModel but with batch normalization and dropout
+    def __init__(self,
+                 input_size: int,
+                 n_classes: int,
+                 hidden_size: list,
+                 drop_out: float,
+                 lr: float,
+                 weight_decay: float, **kwargs):
+        super().__init__(input_size, n_classes, hidden_size, lr, weight_decay)
+        # add batch normalization and replace linear layers
+        layers = []
+        for i in range(len(hidden_size)):
+            if i == 0:
+                layers.append(torch.nn.Sequential(Linear(input_size, hidden_size[i]),
+                                                  torch.nn.BatchNorm1d(hidden_size[i]),
+                                                  torch.nn.Dropout(drop_out)))
+            else:
+                layers.append(torch.nn.Sequential(Linear(hidden_size[i-1], hidden_size[i]),
+                                                  torch.nn.BatchNorm1d(hidden_size[i]),
+                                                  torch.nn.Dropout(drop_out)))
+        layers.append(Linear(hidden_size[-1], n_classes))
+        self.model = torch.nn.Sequential(*layers)
+
 class LendingClubDataset(Dataset):
     def __init__(self, features, labels):
         self.features = features
@@ -84,23 +146,21 @@ def get_dataloader(features, labels, batch_size):
     return DataLoader(dataset, batch_size=batch_size)
 
 def hyperparameter_tuning(model_fn: torch.nn.Module,
-                          input_size: int,
-                          n_classes: int,
-                          hidden_size: list,
+                          model_params: dict,
                           val_X: torch.tensor,
                           val_y: torch.tensor,
                           random_search_itr=100,
                           max_epochs=10,
                           metric='f1'):
     # lr, weight_decay, batch_size, hidden_size
-    trainer = L.Trainer(max_epochs=max_epochs, devices='auto')
     result = {}
     for i in range(random_search_itr):
+        trainer = L.Trainer(max_epochs=max_epochs, devices='auto', )
         # create model
         lr = 10 ** (-5 * torch.rand(1)).item()
-        weight_decay = 10 ** (-5 * torch.rand(1)) .item()
-        batch_size = (2 ** torch.randint(1, 8, (1,))).item() # 2 - 258
-        model = model_fn(input_size, n_classes, hidden_size, lr=lr, weight_decay=weight_decay)
+        weight_decay = 10 ** (-5 * torch.rand(1)).item()
+        batch_size = (2 ** torch.randint(4, 7, (1,))).item() # 16 - 128
+        model = model_fn(**model_params, lr=lr, weight_decay=weight_decay)
         val_loader = get_dataloader(val_X, val_y, batch_size)
         # train and evalute on validation set
         trainer.fit(model, val_loader)
@@ -119,9 +179,7 @@ def hyperparameter_tuning(model_fn: torch.nn.Module,
         
 
 def pipeline(model_fn: torch.nn.Module,
-             input_size: int,
-             n_classes: int,
-             hidden_size: list,
+             model_params: dict,
              train_X: torch.tensor,
              train_y: torch.tensor,
              test_X: torch.tensor,
@@ -137,9 +195,7 @@ def pipeline(model_fn: torch.nn.Module,
         train_X, train_y, test_size=validation_split, stratify=train_y)
     # hyperparameter tuning
     hyperparam = hyperparameter_tuning(model_fn,
-                                       input_size,
-                                       n_classes,
-                                       hidden_size,
+                                       model_params,
                                        val_X,
                                        val_y,
                                        random_search_itr,
@@ -151,7 +207,7 @@ def pipeline(model_fn: torch.nn.Module,
     test_loader = get_dataloader(test_X, test_y, hyperparam['batch_size'])
     # set up trainer
     trainer = L.Trainer(max_epochs=n_epochs, devices=device)
-    model = model_fn(input_size, n_classes, hidden_size, **hyperparam)
+    model = model_fn(**model_params, **hyperparam)
     trainer.fit(model, train_loader, val_dataloaders=val_loader)
     # train
     trainer.test(model, test_loader)
@@ -161,22 +217,43 @@ def unit_test_pipeline():
     y_train = torch.randint(0, 2, (100,))
     X_test = torch.randn(100, 10)
     y_test = torch.randint(0, 2, (100,))
-    pipeline(LinearModel,
-         input_size=X_train.shape[1],
-         n_classes=2,
-         hidden_size=[128,128],
-         train_X=X_train,
-         train_y=y_train,
-         test_X=X_test,
-         test_y= y_test,
-         n_epochs=5,
-         device='auto',
-         validation_split=0.2,
-         random_search_itr=10,
-         hyperparam_metric='f1',
-         hyperparam_epoch=10
-         )
+    model_params = {"input_size": X_train.shape[1], "n_classes": 2,"hidden_size": [128, 128]}
+    pipeline(
+        LinearModel,
+        model_params,
+        train_X=X_train,
+        train_y=y_train,
+        test_X=X_test,
+        test_y= y_test,
+        n_epochs=5,
+        device='auto',
+        validation_split=0.2,
+        random_search_itr=10,
+        hyperparam_metric='f1',
+        hyperparam_epoch=10)
+
+def unit_test_models():
+    X_train = torch.randn(100, 10)
+    y_train = torch.randint(0, 2, (100,))
+    X_test = torch.randn(100, 10)
+    y_test = torch.randint(0, 2, (100,))
+    model_params = {"input_size": X_train.shape[1], "n_classes": 2,"hidden_size": [128, 128], 'drop_out': 0.2}
+    pipeline(
+        LinearModelBatchNormDropout,
+        model_params,
+        train_X=X_train,
+        train_y=y_train,
+        test_X=X_test,
+        test_y= y_test,
+        n_epochs=5,
+        device='auto',
+        validation_split=0.2,
+        random_search_itr=10,
+        hyperparam_metric='f1',
+        hyperparam_epoch=10)
+
 
 if __name__ == "__main__":
     #unit_test_pipeline()
-    unit_test_pipeline()
+    #unit_test_pipeline()
+    unit_test_models()
